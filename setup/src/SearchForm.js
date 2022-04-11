187d4f28e1dfcfcedd813b2d4c09b55169054e5a
Muhammad Asif <asif88383@gmail.com>
2022-04-12 00:02:04 +0500

    first commit

--- setup/src/SearchForm.js
+++ setup/src/SearchForm.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import { useGlobalContext } from './context'
+
 const SearchForm = () => {
-  return <h2>search component</h2>
+  const {query, setQuery, error} = useGlobalContext() // getting data from context and setting it to variables
+
+  return (
+    <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+      <h2>Search Movies</h2>
+      <input
+        type='text'
+        placeholder='Search...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className='form-input'
+      />
+      {error.show && <div className='error'>{error.msg}</div>}
+    </form>
+  )
 }
 
 export default SearchForm
